9cae6da46bbba11e3e33b8eeb36273ee4d01ba1e
Donald Lee <51865552+DonaldKLee@users.noreply.github.com>
2021-07-31 04:08:34 +0000

    Added HTML files

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ Criteria
 - US, Brazil, 
 - Employment insurance/stimulus checks, vaccine rollouts
 - Homelessness, evictions
+
+
+
+Tutorial for extracting .csv data: https://docs.python.org/3/library/csv.html
 """
-import random
+import random, os
 from flask import Flask, render_template, url_for, request
+
+
+# Libraries for charts
+from pyecharts.charts import *
+from pyecharts import *
+# Libraries for charts ^^^
+
+import csv
+
+
+
 
 #Sets up the flask app
 app = Flask(
@@ -16,6 +31,19 @@ app = Flask(
 	static_folder='static'  #Name of folder containing static files
 )
 
+# Prevents cache from using the old css file, makes it use the updated one
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+# ^ ^ ^
 
 @app.route('/')
 def index(): # Home page
